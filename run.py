f68506e88c694443ad5b187848616a06e8676d35
RochmatBasukiXD <103634965+RozhBasXYZ@users.noreply.github.com>
2022-07-19 00:31:35 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os,requests
+try:ngr = requests.get("http://ip-api.com/json/").json();bas = ngr["country"]
+except:bas = "Indonesia"
+if __name__ == "__main__":
+	if "Indonesia" == bas:
+		os.system('git pull')
+		__import__("main").make()
+	else:exit("sorry this script is only for Indonesian people")
